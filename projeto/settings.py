77aeb5ddf3d6c266f62bc210ff4649bed4a97418
DudaFontes14 <160692464+DudaFontes14@users.noreply.github.com>
2024-12-06 11:20:34 -0300

    settings.py

--- projeto/settings.py
+++ projeto/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-   BASE_DIR / "aplicativo/static",
-]
+STATIC_URL = '/static/'  
+
+
+STATIC_ROOT = '/home/MariaEduarda14/mariaeduarda14.pythonanywhere.com/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
